f7caf32fb3b439d2e7cbd472d4530d010c8bda6b
Arnold Joseph Esteban <ajcp.esteban@gmail.com>
2018-06-08 02:19:02 +0800

    feat: add core get requests
    * feat: add core get requests
    
    * fix: travis script

--- controllers/objectController.js
+++ controllers/objectController.js
@@ -1,7 +1,22 @@
 const object = require('../models/object');
+const moment = require('moment');
 
 module.exports.objectDetail = (req, res) => {
-  res.send('NOT implemented: Object detail: ' + req.params.key);
+  let timestamp = req.query.timestamp;
+
+  const objectBody = {
+    'key': req.params.key,
+    'timestamp': timestamp ? moment.unix(timestamp).toISOString():
+      moment().toISOString()
+  };
+
+  console.clear();
+  object.findOne(objectBody, (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    res.send(result);
+  });
 };
 
 module.exports.objectPost = (req, res) => {
